84e35ef6c64fd8b7440485579793592e5cf52545
Jannatul Chhoa <jchhoa@sabine.rcdc.uh.edu>
2023-06-13 16:03:14 -0500

    ++

--- run-malpem-all-sync.sh
+++ run-malpem-all-sync.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
-#DIR=/project/mang/chhoa/code/malpem
+CDIR=/project/mang/chhoa/code/malpem
 DDIR=/project/mang/data/ADNI_013batch013_S_1035
 
 for subfolder in ${DDIR}/*; do
 mkdir -p "${subfolder}/Seg_MALPEM"
+
  for file in ${subfolder}/*.nii; do
- fname=$(basename "$file")  
-  for folder in "${subfolder}/Seg_MALPEM"/*; do 
-    if [ "$fname" != "${folder%%[.]*}" ]; then
-    echo "$file"
+ fname=$(basename "$file" .0.nii)
+  match=0
+
+  for folder in "${subfolder}/Seg_MALPEM"/*; do
+  fdname=$(basename "${folder%%[.]*}")
+    if [ "$fname" = "$fdname" ];
+    then match=1
     fi
   done
- done 
+
+  if [ $match = 0 ];then
+
+### submit the job
+sbatch <<EOF
+
+#!/bin/bash
+#SBATCH -J malpem
+#SBATCH -N 1 -n 40
+#SBATCH --mem=128GB
+#SBATCH --output=/project/mang/chhoa/scripts/log/run-.%x.%j.out
+#SBATCH --error=/project/mang/chhoa/scripts/log/run-.%x.%j.err
+#SBATCH -t 01:00:00
+#SBATCH --mail-user=jchhoa@uh.edu
+#SBATCH --mail-type=fail
+#SBATCH -A mang
+
+### run the malpem-proot command
+${CDIR}/bin/malpem-proot -i ${file} -o ${subfolder}/Seg_MALPEM --threads 40
+#echo "$file"
+
+EOF
+
+  fi
+done
 done
--- run-sync-try.sh
+++ run-sync-try.sh
@@ -7,17 +7,20 @@ for subfolder in ${DDIR}/*; do
 mkdir -p "${subfolder}/Seg_MALPEM" 
  for file in ${subfolder}/*.nii; do
  fname=$(basename "$file" .0.nii)
- i=0; j=0
+ #i=0; j=0
+ match=0 
   for folder in "${subfolder}/Seg_MALPEM"/*; do
-  j=$((j + 1)) 
+  #j=$((j + 1)) 
   fdname=$(basename "${folder%%[.]*}")
     if [ "$fname" = "$fdname" ];
-    then break;
-    else i=$((i + 1))
+    #then break;
+    #else i=$((i + 1))
+    then match=1
     fi
   done
-  echo $i $j
-  if [ $i = $j ]; then
+  #echo $i $j
+  #if [ $i = $j ]; then
+  if [ $match = 0 ]; then
   echo "$file"
   fi
 done 
