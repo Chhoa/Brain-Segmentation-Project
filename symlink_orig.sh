0120cd172667dced7b68da5a05268841f0891068
Jannatul Chhoa <jchhoa@sabine.rcdc.uh.edu>
2023-06-13 23:06:31 -0500

    ++

--- symlink_orig.sh
+++ symlink_orig.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-dir="/project/mang/data/ADNI_013batch"
+dir="/project/mang/data/ADNI_014results"
 
 for d1 in "$dir"/*; do
   for d2 in "$d1"/M*; do
-  #if [ "$d2" != "$Seg_MALPEM" ]; then
+  #if [ "$d2" != "Seg_MALPEM" ]; then
     for d3 in "$d2"/*; do
       for d4 in "$d3"/*; do
         for file in "$d4"/*; do
