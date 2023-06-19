06c166e274e4ab0390b2168f0f61e417a683bc72
Jannatul Chhoa <jchhoa@sabine.rcdc.uh.edu>
2023-06-19 00:13:58 -0500

    update

--- segExtractRLV_ML-mod_sbatch.sh
+++ segExtractRLV_ML-mod_sbatch.sh
@@ -14,7 +14,13 @@ module load matlab
 dir="/project/mang/data/ADNI_batches/batch_1"
 
 for d1 in "$dir"/*; do
-      for file in "$d1"/*_MALPEM.nii.gz; do
+    for file in "$d1"/*_MALPEM.nii.gz; do
+    fname=$(basename "$file" .nii.gz)
+    vname=LV_$fname-cca.nii.gz
+      
+      if [ ! -f $d1/$vname ]; then
       matlab -nodisplay -batch "addpath('/project/mang/chhoa/MLtoolbox/NIfTI_20140122'); segmentExtractRLV_CCA_mod('$file', '$d1'); exit"
+      fi
+    
     done
 done
