87473235c12491feedb6491ebfee75f82707e07b
Jannatul Chhoa <jchhoa@sabine.rcdc.uh.edu>
2023-06-21 02:50:12 -0500

    ++

--- cp_loop.sh
+++ cp_loop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ### directory of code
-SDIR=/project/mang/data/ADNI_batches
-DDIR=/project/mang/data/ADNI_ventricles
+SDIR=/project/mang/data/ADNI_data+results
+DDIR=/project/mang/data/ADNI_vnew
 
 ### loop over each folder
 for folder in "$SDIR"/*
@@ -10,17 +10,17 @@ do
     folder_name=$(basename "$folder")
     ### create the output directory if it does not exist
     mkdir -p "${DDIR}/${folder_name}"
-    for folder2 in "$folder"/*
-    do
-        folder2_name=$(basename "$folder2")
-        mkdir -p "${DDIR}/${folder_name}/${folder2_name}"
-        for file in "$folder2"/*vtk
+    #for folder2 in "$folder"/*
+    #do
+        #folder2_name=$(basename "$folder2")
+        #mkdir -p "${DDIR}/${folder_name}/${folder2_name}"
+        for file in "$folder"/*vtk
         do
             ### check if the item is a file
             if [ -f "${file}" ]
             then
-                cp "${file}" "${DDIR}/${folder_name}/${folder2_name}"
+                cp -v "${file}" "${DDIR}/${folder_name}"
             fi
         done
-    done
+    #done
 done
